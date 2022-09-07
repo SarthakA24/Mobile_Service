c82c3c44f69eb24332d32f93b47c584aceb2992d
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-07 22:24:47 +0530

    removed the unnecessary numberOfLines counter code

--- src/main/java/com/sarthak/MobileStore.java
+++ src/main/java/com/sarthak/MobileStore.java
@@ -14,18 +14,6 @@ import java.util.List;
 public class MobileStore extends Mobile {
     public List<Mobile> readMobileData(String fileName) {
         List<Mobile> mobileList = new ArrayList<>();
-        // Create a variable to count the number of line in the file
-        int numberOfLines = 0;
-        // Create an instance of BufferedReader and FileReader in a try-with-resource block to count the number of
-        // line in the file
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
-            bufferedReader.readLine();
-            while (bufferedReader.readLine() != null) {
-                numberOfLines++;
-            }
-        } catch (IOException exception) {
-            System.err.println(exception.getMessage());
-        }
         // Read the data from the file and store it in the List mobileList
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             String line = bufferedReader.readLine();
