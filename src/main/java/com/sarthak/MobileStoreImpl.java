e4972b5ca0a12f7c498c93425b1885a034106082
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-07 23:15:47 +0530

    wrote the main method to call various methods and perform the operations

--- src/main/java/com/sarthak/MobileStoreImpl.java
+++ src/main/java/com/sarthak/MobileStoreImpl.java
@@ -5,8 +5,19 @@
  */
 package com.sarthak;
 
+import java.util.List;
+
 public class MobileStoreImpl {
     public static void main(String[] args) {
-
+        MobileStore mobileStore = new MobileStore();
+        String fileName = "src/main/resources/mobile.csv";
+        List<Mobile> mobileList = mobileStore.readMobileData(fileName);
+        System.out.println("mobileList = " + mobileList);
+        List<Mobile> oneplus = mobileStore.findPhoneByBrand("oneplus");
+        System.out.println("oneplus = " + oneplus);
+        List<Mobile> phoneCostMoreThan$500 = mobileStore.findPhoneCostMoreThan$500();
+        System.out.println("phoneCostMoreThan$500 = " + phoneCostMoreThan$500);
+        List<Mobile> phonePixelMoreThan12MP = mobileStore.findPhonePixelMoreThan12MP();
+        System.out.println("phonePixelMoreThan12MP = " + phonePixelMoreThan12MP);
     }
 }
