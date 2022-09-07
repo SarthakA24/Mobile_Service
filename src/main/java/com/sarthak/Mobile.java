af665e55dbc4b3094a8f5e6178d2419abd64f994
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-07 19:52:19 +0530

    added the getters and setters methods

--- src/main/java/com/sarthak/Mobile.java
+++ src/main/java/com/sarthak/Mobile.java
@@ -10,4 +10,36 @@ public class Mobile {
     private double cost;
     private double screenSize;
     private int batteryLife;
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public double getScreenSize() {
+        return screenSize;
+    }
+
+    public void setScreenSize(double screenSize) {
+        this.screenSize = screenSize;
+    }
+
+    public int getBatteryLife() {
+        return batteryLife;
+    }
+
+    public void setBatteryLife(int batteryLife) {
+        this.batteryLife = batteryLife;
+    }
 }
