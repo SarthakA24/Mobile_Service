ff231d295cf97d4e69ee1f24356f2ce314148ee4
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-07 22:39:37 +0530

    added a field of megaPixels and changed the necessary constructors, getter, setter, hashCode, equals and toString methods

--- src/main/java/com/sarthak/Mobile.java
+++ src/main/java/com/sarthak/Mobile.java
@@ -12,6 +12,7 @@ public class Mobile {
     private double cost;
     private double screenSize;
     private int batteryLife;
+    private int megaPixels;
 
     public String getBrandName() {
         return brandName;
@@ -45,11 +46,12 @@ public class Mobile {
         this.batteryLife = batteryLife;
     }
 
-    public Mobile(String brandName, double cost, double screenSize, int batteryLife) {
+    public Mobile(String brandName, double cost, double screenSize, int batteryLife, int megaPixels) {
         this.brandName = brandName;
         this.cost = cost;
         this.screenSize = screenSize;
         this.batteryLife = batteryLife;
+        this.megaPixels = megaPixels;
     }
 
     public Mobile() {
@@ -62,6 +64,7 @@ public class Mobile {
                 ", cost=" + cost +
                 ", screenSize=" + screenSize +
                 ", batteryLife=" + batteryLife +
+                ", megaPixels=" + megaPixels +
                 '}';
     }
 
@@ -70,11 +73,20 @@ public class Mobile {
         if (this == o) return true;
         if (!(o instanceof Mobile)) return false;
         Mobile mobile = (Mobile) o;
-        return Double.compare(mobile.getCost(), getCost()) == 0 && Double.compare(mobile.getScreenSize(), getScreenSize()) == 0 && getBatteryLife() == mobile.getBatteryLife() && getBrandName().equals(mobile.getBrandName());
+        return Double.compare(mobile.getCost(), getCost()) == 0 && Double.compare(mobile.getScreenSize(), getScreenSize()) == 0 && getBatteryLife() == mobile.getBatteryLife() && getMegaPixels() == mobile.getMegaPixels() && getBrandName().equals(mobile.getBrandName());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getBrandName(), getCost(), getScreenSize(), getBatteryLife());
+        return Objects.hash(getBrandName(), getCost(), getScreenSize(), getBatteryLife(), getMegaPixels());
     }
+
+    public int getMegaPixels() {
+        return megaPixels;
+    }
+
+    public void setMegaPixels(int megaPixels) {
+        this.megaPixels = megaPixels;
+    }
+
 }
