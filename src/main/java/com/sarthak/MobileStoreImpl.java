9c5845936138bbb6c6bff4fd98df0c99c9e879fb
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-09 23:44:46 +0530

    added the switch cases inside the do while loop to repeat the menu

--- src/main/java/com/sarthak/MobileStoreImpl.java
+++ src/main/java/com/sarthak/MobileStoreImpl.java
@@ -5,18 +5,46 @@
  */
 package com.sarthak;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class MobileStoreImpl {
     public static void main(String[] args) {
         MobileStore mobileStore = new MobileStore();
+        String repeat;
         Scanner scanner = new Scanner(System.in);
-        mobileStore.displayMenu();
-        int choice = Integer.parseInt(scanner.nextLine());
-        // Read the mobile data from the file
-        mobileStore.readMobileData();
-        // Display all the mobiles available in the store to the customer in Alphabetically order
-        mobileStore.sortAlphabetically();
-        mobileStore.displayMobile();
+        do {
+            mobileStore.displayMenu();
+            int choice = Integer.parseInt(scanner.nextLine());
+            switch (choice) {
+                case 1:
+                    mobileStore.readMobileData();
+                    mobileStore.sortAlphabetically();
+                    mobileStore.displayMobile();
+                    break;
+                case 2:
+                    mobileStore.addMobileToList();
+                    System.out.println("Mobile has been added to the List!!");
+                    break;
+                case 3:
+                    System.out.println("Enter the brand name to search the mobile(s) - ");
+                    String brandName = scanner.nextLine();
+                    List<Mobile> phoneByBrand = mobileStore.findPhoneByBrand(brandName);
+                    mobileStore.displayMobile(phoneByBrand,brandName);
+                    break;
+                case 4:
+                    mobileStore.readMobileData();
+                    mobileStore.sortBasedOnCost();
+                    mobileStore.displayMobile();
+                    break;
+                case 5:
+                    break;
+                default:
+                    System.err.println("Invalid Input!! Please try again");
+
+            }
+            System.out.println("Do you want to perform more operation? (y/n) - ");
+            repeat = scanner.nextLine();
+        } while (repeat.equalsIgnoreCase("y"));
     }
 }
