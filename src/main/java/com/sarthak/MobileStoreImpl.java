60db8d9668987485b2d089e1a802c3d56c519e55
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-09 23:26:24 +0530

    displayed the menu to the user

--- src/main/java/com/sarthak/MobileStoreImpl.java
+++ src/main/java/com/sarthak/MobileStoreImpl.java
@@ -5,9 +5,14 @@
  */
 package com.sarthak;
 
+import java.util.Scanner;
+
 public class MobileStoreImpl {
     public static void main(String[] args) {
         MobileStore mobileStore = new MobileStore();
+        Scanner scanner = new Scanner(System.in);
+        mobileStore.displayMenu();
+        int choice = Integer.parseInt(scanner.nextLine());
         // Read the mobile data from the file
         mobileStore.readMobileData();
         // Display all the mobiles available in the store to the customer in Alphabetically order
