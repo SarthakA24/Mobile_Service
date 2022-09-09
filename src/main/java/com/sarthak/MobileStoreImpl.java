1ebd3d47c08c8d396372bfe3c92d5d25f48ab05c
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-09 23:21:28 +0530

    read the data from the file, sorted alphabetically and displayed the details

--- src/main/java/com/sarthak/MobileStoreImpl.java
+++ src/main/java/com/sarthak/MobileStoreImpl.java
@@ -10,6 +10,10 @@ import java.util.List;
 public class MobileStoreImpl {
     public static void main(String[] args) {
         MobileStore mobileStore = new MobileStore();
-
+        // Read the mobile data from the file
+        mobileStore.readMobileData();
+        // Display all the mobiles available in the store to the customer in Alphabetically order
+        mobileStore.sortAlphabetically();
+        mobileStore.displayMobile();
     }
 }
