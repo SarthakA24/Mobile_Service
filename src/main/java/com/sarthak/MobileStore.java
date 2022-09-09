46653cbc3976c0b10251d659364f6ae88d4fd52e
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-09 23:25:51 +0530

    added the method to display the menu

--- src/main/java/com/sarthak/MobileStore.java
+++ src/main/java/com/sarthak/MobileStore.java
@@ -98,4 +98,15 @@ public class MobileStore {
     public void sortAlphabetically() {
         mobileList.sort(new MobileComparatorAlphabetically());
     }
+
+    public void displayMenu(){
+        System.out.println("Welcome to the Mobile Store!!!");
+        System.out.println("Please select the options from below - ");
+        System.out.println("1. Display all the Available Mobiles in the shop");
+        System.out.println("2. Add Mobile to the shop");
+        System.out.println("3. Find a specific brand of mobile - ");
+        System.out.println("4. Display mobiles with Price low to high - ");
+        System.out.println("5. Exit the shop - ");
+        System.out.println("Please enter your choice (1-5) - ");
+    }
 }
