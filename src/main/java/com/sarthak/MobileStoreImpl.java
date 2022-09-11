4bb516ebcd477a3bea81bff47e5381c96d90a07e
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-11 19:07:52 +0530

    added the do while loop in try catch blocks to handle any exception in inputs

--- src/main/java/com/sarthak/MobileStoreImpl.java
+++ src/main/java/com/sarthak/MobileStoreImpl.java
@@ -13,36 +13,39 @@ public class MobileStoreImpl {
         String repeat;
         Scanner scanner = new Scanner(System.in);
         do {
-            mobileStore.displayMenu();
-            int choice = Integer.parseInt(scanner.nextLine());
-            switch (choice) {
-                case 1:
-                    mobileStore.sortAlphabetically();
-                    mobileStore.displayMobile();
-                    break;
-                case 2:
-                    mobileStore.addMobileToList();
-                    System.out.println("Mobile has been added to the List!!");
-                    break;
-                case 3:
-                    System.out.println("Enter the Brand Name - ");
-                    String mobileToRemove = scanner.nextLine();
-                    mobileStore.removePhoneFromList(mobileToRemove);
-                    break;
-                case 4:
-                    System.out.println("Enter the brand name to search the mobile(s) - ");
-                    String brandName = scanner.nextLine();
-                    mobileStore.displayMobile(brandName);
-                    break;
-                case 5:
-                    mobileStore.sortBasedOnCost();
-                    mobileStore.displayMobile();
-                    break;
-                case 6:
-                    break;
-                default:
-                    System.err.println("Invalid Input!! Please try again");
-
+            try {
+                mobileStore.displayMenu();
+                int choice = Integer.parseInt(scanner.nextLine());
+                switch (choice) {
+                    case 1:
+                        mobileStore.sortAlphabetically();
+                        mobileStore.displayMobile();
+                        break;
+                    case 2:
+                        mobileStore.addMobileToList();
+                        System.out.println("Mobile has been added to the List!!");
+                        break;
+                    case 3:
+                        System.out.println("Enter the Brand Name - ");
+                        String mobileToRemove = scanner.nextLine();
+                        mobileStore.removePhoneFromList(mobileToRemove);
+                        break;
+                    case 4:
+                        System.out.println("Enter the brand name to search the mobile(s) - ");
+                        String brandName = scanner.nextLine();
+                        mobileStore.displayMobile(brandName);
+                        break;
+                    case 5:
+                        mobileStore.sortBasedOnCost();
+                        mobileStore.displayMobile();
+                        break;
+                    case 6:
+                        break;
+                    default:
+                        System.err.println("Invalid Input!! Please try again");
+                }
+            } catch (Exception exception) {
+                System.err.println("Incorrect Input!! Please enter the correct choice");
             }
             System.out.println("Do you want to perform more operation? (y/n) - ");
             repeat = scanner.nextLine();
